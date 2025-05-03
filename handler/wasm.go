ae509c4979b6aa208da9ef93af2f00335dae2ce2
cn1095 <1095053105@qq.com>
2025-05-03 20:32:11 +0800

    Update wasm.go

--- handler/wasm.go
+++ handler/wasm.go
@@ -2,7 +2,7 @@ package handler
 
 import (
 	"path/filepath"
-
+	"io/fs"
 	"github.com/cn1095/hit-counter/internal"
 	"github.com/labstack/echo/v4"
 )
@@ -11,5 +11,12 @@ import (
 func (h *Handler) Wasm(c echo.Context) error {
 	hctx := c.(*HitCounterContext)
 	hctx.Response().Header().Set("Content-Encoding", "gzip")
-	return c.File(filepath.Join(internal.GetRoot(), "view", "hits.wasm"))
+	// 从嵌入的文件系统中读取 wasm 文件
+	data, err := fs.ReadFile(EmbeddedFiles, "view/hits.wasm")
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "无法读取 wasm 文件")
+	}
+
+	// 返回文件内容作为响应
+	return c.Blob(http.StatusOK, "application/wasm", data)
 }
