79dacfa56d8417fdfc2e65e3fed5c55a0decf83e
cn1095 <1095053105@qq.com>
2025-11-30 11:17:00 +0800

    Update handler.go

--- handler/handler.go
+++ handler/handler.go
@@ -1,120 +1,121 @@
-package handler
-
-import (
-	"fmt"
-	"html/template"
-	"path/filepath"
-	"runtime"
-	"time"
-
-	task "github.com/cn1095/go-async-task"
-	badge "github.com/cn1095/go-counter-badge/badge"
-	websocket "github.com/cn1095/go-ws-broadcast"
-	"github.com/cn1095/hit-counter/counter"
-	"github.com/cn1095/hit-counter/env"
-	"github.com/cn1095/hit-counter/internal"
-	"github.com/go-redis/redis/v8"
-	cache "github.com/patrickmn/go-cache"
-)
-
-var (
-	iconsMap  map[string]badge.Icon
-	iconsList []map[string]string
-)
-
-type Handler struct {
-	Counter          counter.Counter
-	LocalCache       *cache.Cache
-	AsyncTask        task.Keeper
-	WebSocketBreaker websocket.Breaker
-	IndexTemplate    *template.Template
-	Badge            badge.Writer
-	Icons            map[string]badge.Icon
-	IconsList        []map[string]string
-}
-
-// NewHandler creates  handler object.
-func NewHandler(redisAddr string) (*Handler, error) {
-	if redisAddr == "" {
-		return nil, fmt.Errorf("[错误] NewHandler %w", internal.ErrorEmptyParams)
-	}
-
-	// create local cache
-	localCache := cache.New(24*time.Hour, 10*time.Minute)
-
-	redisClient := redis.NewClient(&redis.Options{
-		Addr:         redisAddr,
-		Password:     env.GetRedisPassword(),
-		DB:           0,
-		MaxRetries:   1,
-		MinIdleConns: runtime.NumCPU() * 3,
-		PoolSize:     runtime.NumCPU() * 10,
-	})
-	ctr, err := counter.NewCounter(counter.WithRedisClient(redisClient))
-	if err != nil {
-		return nil, fmt.Errorf("[错误] NewHandler %w", err)
-	}
-
-	// create async task
-	asyncTask, err := task.NewAsyncTask(
-		task.WithQueueSizeOption(1000),
-		task.WithWorkerSizeOption(5),
-		task.WithTimeoutOption(20*time.Second),
-		task.WithErrorHandlerOption(func(err error) {
-			internal.SentryError(err)
-		}),
-	)
-	if err != nil {
-		return nil, fmt.Errorf("[错误] NewHandler %w", err)
-	}
-
-	// create websocket breaker
-	breaker, err := websocket.NewBreaker(websocket.WithMaxReadLimit(1024),
-		websocket.WithMaxMessagePoolLength(500),
-		websocket.WithErrorHandlerOption(func(err error) {
-			internal.SentryError(err)
-		}))
-	if err != nil {
-		return nil, fmt.Errorf("[错误] NewHandler %w", err)
-	}
-
-	// template
-	indexName := "local.html"  
-	if env.GetPhase() == "production" && env.GetPhase() != "local" {  
-    	indexName = "index.html"  
-	}
-
-	indexTemplate, err := template.ParseFiles(filepath.Join(internal.GetRoot(), "view", indexName))
-	if err != nil {
-		return nil, fmt.Errorf("[错误] NewHandler %w", err)
-	}
-
-	// badge generator
-	badgeWriter, err := badge.NewWriter()
-	if err != nil {
-		return nil, fmt.Errorf("[错误] NewHandler %w", err)
-	}
-
-	return &Handler{
-		LocalCache:       localCache,
-		Counter:          ctr,
-		AsyncTask:        asyncTask,
-		WebSocketBreaker: breaker,
-		IndexTemplate:    indexTemplate,
-		Badge:            badgeWriter,
-		Icons:            iconsMap,
-		IconsList:        iconsList,
-	}, nil
-}
-
-func init() {
-	iconsMap = badge.GetIconsMap()
-	iconsList = make([]map[string]string, 0, len(iconsMap))
-
-	for k, _ := range iconsMap {
-		j := make(map[string]string, 2)
-		j["name"] = k
-		j["url"] = fmt.Sprintf("/icon/%s", k)
-		iconsList = append(iconsList, j)
-	}
+package handler  
+  
+import (  
+	"fmt"  
+	"html/template"  
+	"runtime"  
+	"time"  
+  
+	task "github.com/cn1095/go-async-task"  
+	badge "github.com/cn1095/go-counter-badge/badge"  
+	websocket "github.com/cn1095/go-ws-broadcast"  
+	"github.com/cn1095/hit-counter/counter"  
+	"github.com/cn1095/hit-counter/env"  
+	"github.com/cn1095/hit-counter/internal"  
+	"github.com/go-redis/redis/v8"  
+	cache "github.com/patrickmn/go-cache"  
+	"github.com/cn1095/hit-counter"  // 导入 main 包  
+)  
+  
+var (  
+	iconsMap  map[string]badge.Icon  
+	iconsList []map[string]string  
+)  
+  
+type Handler struct {  
+	Counter          counter.Counter  
+	LocalCache       *cache.Cache  
+	AsyncTask        task.Keeper  
+	WebSocketBreaker websocket.Breaker  
+	IndexTemplate    *template.Template  
+	Badge            badge.Writer  
+	Icons            map[string]badge.Icon  
+	IconsList        []map[string]string  
+}  
+  
+// NewHandler creates a handler object.  
+func NewHandler(redisAddr string) (*Handler, error) {  
+	if redisAddr == "" {  
+		return nil, fmt.Errorf("[错误] NewHandler %w", internal.ErrorEmptyParams)  
+	}  
+  
+	// create local cache  
+	localCache := cache.New(24*time.Hour, 10*time.Minute)  
+  
+	redisClient := redis.NewClient(&redis.Options{  
+		Addr:         redisAddr,  
+		Password:     env.GetRedisPassword(),  // 使用环境变量中的密码  
+		DB:           0,  
+		MaxRetries:   1,  
+		MinIdleConns: runtime.NumCPU() * 3,  
+		PoolSize:     runtime.NumCPU() * 10,  
+	})  
+	ctr, err := counter.NewCounter(counter.WithRedisClient(redisClient))  
+	if err != nil {  
+		return nil, fmt.Errorf("[错误] NewHandler %w", err)  
+	}  
+  
+	// create async task  
+	asyncTask, err := task.NewAsyncTask(  
+		task.WithQueueSizeOption(1000),  
+		task.WithWorkerSizeOption(5),  
+		task.WithTimeoutOption(20*time.Second),  
+		task.WithErrorHandlerOption(func(err error) {  
+			internal.SentryError(err)  
+		}),  
+	)  
+	if err != nil {  
+		return nil, fmt.Errorf("[错误] NewHandler %w", err)  
+	}  
+  
+	// create websocket breaker  
+	breaker, err := websocket.NewBreaker(websocket.WithMaxReadLimit(1024),  
+		websocket.WithMaxMessagePoolLength(500),  
+		websocket.WithErrorHandlerOption(func(err error) {  
+			internal.SentryError(err)  
+		}))  
+	if err != nil {  
+		return nil, fmt.Errorf("[错误] NewHandler %w", err)  
+	}  
+  
+	// template - 使用嵌入文件系统  
+	indexName := "index.html"  
+	if env.GetPhase() == "local" {  
+		indexName = "local.html"  
+	}  
+  
+	// 从嵌入的文件系统加载模板  
+	indexTemplate, err := template.ParseFS(hitcounter.EmbeddedFiles(), "view/"+indexName)  
+	if err != nil {  
+		return nil, fmt.Errorf("[错误] NewHandler %w", err)  
+	}  
+  
+	// badge generator  
+	badgeWriter, err := badge.NewWriter()  
+	if err != nil {  
+		return nil, fmt.Errorf("[错误] NewHandler %w", err)  
+	}  
+  
+	return &Handler{  
+		LocalCache:       localCache,  
+		Counter:          ctr,  
+		AsyncTask:        asyncTask,  
+		WebSocketBreaker: breaker,  
+		IndexTemplate:    indexTemplate,  
+		Badge:            badgeWriter,  
+		Icons:            iconsMap,  
+		IconsList:        iconsList,  
+	}, nil  
+}  
+  
+func init() {  
+	iconsMap = badge.GetIconsMap()  
+	iconsList = make([]map[string]string, 0, len(iconsMap))  
+  
+	for k, _ := range iconsMap {  
+		j := make(map[string]string, 2)  
+		j["name"] = k  
+		j["url"] = fmt.Sprintf("/icon/%s", k)  
+		iconsList = append(iconsList, j)  
+	}  
 }
